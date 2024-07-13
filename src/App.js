862c493a8bffc3197e86799bf87ac3cc749b8a68
manish <manish.kumar@nestorbird.com>
2024-07-13 19:21:39 +0530

    code save

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
 function App() {
+  const title = "cool mind code"
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div>
+    // <h1>
+    // Cool Mind
+    // </h1>
+    // <h2>
+    // Learn Code
+    // </h2>
+    // </div>
+    // [
+    //   <h1>
+    //   hi cool mind code
+    //   </h1>,
+    //   <h2>
+    //   how to learn cool mind code
+    //   </h2>
+    // ]
+
+    // <React.Fragment>
+    //     <h1>
+    //    hi.... react  sikhni hai 
+    //     </h1>
+    //     <h2>
+    //     cool mind guys
+    //     </h2>
+    //     <h3>
+    //     jaldi learn karnege
+    //     </h3>
+    // </React.Fragment>
+
+    <React.Fragment>
+    <h1> New Name = {title} </h1> 
+    </React.Fragment>
   );
 }
 
